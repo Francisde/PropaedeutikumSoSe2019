16c3b5128f7b2b271be625a6f6a9956a2fff7232
Martin <martin.mehlhose@gmx.de>
2019-06-10 21:38:02 +0200

    Klausur eingefügt

---
+++ Material/Uebungsaufgaben/Klausur/Warteschlage.py
@@ -0,0 +1,16 @@
+# Klasse Warteschlage
+class Warteschlange:
+    #Konstruktor bekommt ein maximum übergeben
+    def __init__(...):
+        self.queue=[] # initialisiere die Warteschlange als leere Liste
+    # ein neues Element soll in die Warteschlange eingefügt werden, solange die Maximalanzahl nicht erreicht ist.
+    def addElement(...):
+        ...
+    # Soll das erste Element der Warteschlage zurückgeben und aus der Warteschlage entfernen
+    def nextElement():
+        ...
+        return job
+
+    # soll die aktuelle Warteschlage als String zurückgeben
+    def __str__(...)
+        ...
